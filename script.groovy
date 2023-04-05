17e555415b4f62ae4ec997ba573fbeac9d7253be
isreal <isrealurephu@gmail.com>
2023-04-05 22:24:37 +0100

    More update

--- script.groovy
+++ script.groovy
@@ -5,10 +5,11 @@ def buildJar() {
 
 def buildImage() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+   echo "building image"
+        withCredentials([usernamePassword(credentialsId: 'dockerhub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+          sh 'docker build -t isrealurephu/demo-app:V1 .'
+          sh "echo $PASS | docker login -u $USER --password-stdin"
+          sh "docker push isrealurephu/demo-app:V1"
     }
 } 
 
